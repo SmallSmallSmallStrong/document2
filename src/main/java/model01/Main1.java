fb867ec9b40fb8710c6156573ced2829ce95a5bd
shizhen <>
2017-12-08 11:31:25 +0800

    优化正则表达式 注释输出语句

--- src/main/java/model01/Main1.java
+++ src/main/java/model01/Main1.java
@@ -1,13 +1,11 @@
 package model01;
 
+import model03.Main3;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.ArrayList;
@@ -16,18 +14,39 @@ import java.util.List;
 import java.util.Map;
 
 public class Main1 {
+
+    public static final String PAK_PATH = "D:\\文档内容批量处理程序\\";
     //第一步替换查找的内容
     public static final String REGEX = "1. A";
     //附件一的文档文件夹系统路径
-    public static final String WORD1 = "C:\\Users\\sz\\Desktop\\5-文档内容批量处理程序(1)\\1-文档";
+    public static final String WORD1 = PAK_PATH + "1-文档";
     //附件三的文档文件夹的系统路径
-    public static final String WORD3 = "C:\\Users\\sz\\Desktop\\5-文档内容批量处理程序(1)\\3-需要提取的第一条信息";
+    public static final String WORD3 = PAK_PATH + "3-需要提取的第一条信息";
     //输出路径
-    public static final String OUT = "C:\\Users\\sz\\Desktop\\5-文档内容批量处理程序(1)\\1-out";
+    public static final String OUT = PAK_PATH + "out";
     //关系文件
-    public static final String CONTENT = "C:\\Users\\sz\\Desktop\\5-文档内容批量处理程序(1)\\2-文档中第一条信息选择.xlsx";
+    public static final String CONTENT = PAK_PATH + "2-文档中第一条信息选择.xlsx";
+    //替换内容
+    public static final String REPLACEEXCEL = PAK_PATH + "4-替换内容.xlsx";
+    //替换文本A
+    public static final String REPLACE_TEXT_A = " <C>frame<C> ";
+    public static final String REPLACE_TEXT_B = "<J>Steel building<J>";
+
 
     public static void main(String[] args) {
+        if (one()) {
+            System.out.println("第一步执行成功");
+            if (two()) {
+                System.out.println("第二步执行成功");
+                if (three()) {
+                    System.out.println("第三步执行成功");
+                } else System.err.println("第三步执行失败");
+            } else System.err.println("第二步执行失败");
+        } else System.err.println("第一步执行失败");
+
+    }
+
+    public static boolean one() {
         Map<String, List<String>> map1 = new HashMap<>();
         //读取附件 1的文档
         //文件筛选过滤器
@@ -46,6 +65,7 @@ public class Main1 {
             guanxilist.addAll(ReadExcel.readExcel(excelpath));
         } else {
             System.err.println("文件不存在");
+            return false;
         }
         //读取附件3需要替换的内容
         Path path3 = Paths.get(WORD3);
@@ -86,11 +106,83 @@ public class Main1 {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println(i[0]++ + "" + s);
+            i[0]++;
+//            System.out.println(+ "" + s);
         });
-
-
+        return true;
     }
 
+    public static boolean two() {
+        //读取附件4 - excel文档
+        List<String> list = ReadExcel.readExcel(Paths.get(REPLACEEXCEL));
+        if (list == null || list.size() == 0) return false;
+        final int[] i = {1};
+        list.forEach(s -> {
+            String[] ss = s.split(ReadExcel.SPLITTER);
+            if (ss.length == 2) {
+                //进行替换操作
+                if ("A".equals(ss[1].toUpperCase())) {
+                    try {
+                        ReadWord.replaceAll(Paths.get(Main1.OUT + "\\" + i[0] + ".docx"), REPLACE_TEXT_A, ss[0]);
+                        ReadWord.replaceAll(Paths.get(Main1.OUT + "\\" + i[0] + ".docx"), REPLACE_TEXT_B, "Steel building");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if ("B".equals(ss[1].toUpperCase())) {
+                    try {
+                        ReadWord.replaceAll(Paths.get(Main1.OUT + "\\" + i[0] + ".docx"), REPLACE_TEXT_B, ss[0]);
+                        ReadWord.replaceAll(Paths.get(Main1.OUT + "\\" + i[0] + ".docx"), REPLACE_TEXT_A, "steel frame");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                i[0]++;
+            } else {
+                return;
+            }
+        });
 
+        if (i[0] < list.size()) System.out.println("替换文件数为:" + i[0]);
+
+        return true;
+    }
+
+    public static boolean three() {
+        Map<String, XWPFDocument> list = null;
+        try {
+            list = ReadWord.readDocxs(Paths.get(Main1.OUT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        list.forEach((s, xwpfDocument) -> {
+//            System.out.println(s);
+            List<XWPFParagraph> parlist = xwpfDocument.getParagraphs();
+            parlist.forEach(paragraph -> {
+                String str = paragraph.getText();
+                List<XWPFRun> runslist = paragraph.getRuns();
+                if (str.matches(Main3.REGEX)) {
+                    runslist.forEach(xwpfRun -> {
+                        xwpfRun.setBold(true);
+                    });
+                }
+                runslist.forEach(xwpfRun -> {
+                    xwpfRun.setFontFamily("arial");
+                    xwpfRun.setFontSize(11);
+                });
+            });
+
+            FileOutputStream fos = null;
+            try {
+                fos = new FileOutputStream(Paths.get(Main1.OUT + "\\" + s).toFile());
+                xwpfDocument.write(fos);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        return true;
+    }
 }
--- src/main/java/model03/Main3.java
+++ src/main/java/model03/Main3.java
@@ -12,37 +12,36 @@ import java.util.List;
 import java.util.Map;
 
 public class Main3 {
-    public static String REGEX = "\\d?[.][\\s\\w\\W]+:";
-
-    public static void main(String[] args) throws IOException {
-        Map<String, XWPFDocument> list = ReadWord.readDocxs(Paths.get(Main1.OUT));
-        list.forEach((s, xwpfDocument) -> {
-            System.out.println(s);
-            List<XWPFParagraph> parlist = xwpfDocument.getParagraphs();
-            parlist.forEach(paragraph -> {
-                String str = paragraph.getText();
-                List<XWPFRun> runslist = paragraph.getRuns();
-                if (str.matches(REGEX)) {
-                    runslist.forEach(xwpfRun -> {
-                        xwpfRun.setBold(true);
-                    });
-                }
-                runslist.forEach(xwpfRun -> {
-                    xwpfRun.setFontFamily("arial");
-                    xwpfRun.setFontSize(11);
-                });
-            });
-
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream(Paths.get(Main1.OUT + "\\" + s).toFile());
-                xwpfDocument.write(fos);
-                fos.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-    }
+    public static String REGEX = "^\\d+[.][\\s\\w\\W]+:\\s*$";
+//    public static void main(String[] args) throws IOException {
+//        Map<String, XWPFDocument> list = ReadWord.readDocxs(Paths.get(Main1.OUT));
+//        list.forEach((s, xwpfDocument) -> {
+//            System.out.println(s);
+//            List<XWPFParagraph> parlist = xwpfDocument.getParagraphs();
+//            parlist.forEach(paragraph -> {
+//                String str = paragraph.getText();
+//                List<XWPFRun> runslist = paragraph.getRuns();
+//                if (str.matches(REGEX)) {
+//                    runslist.forEach(xwpfRun -> {
+//                        xwpfRun.setBold(true);
+//                    });
+//                }
+//                runslist.forEach(xwpfRun -> {
+//                    xwpfRun.setFontFamily("arial");
+//                    xwpfRun.setFontSize(11);
+//                });
+//            });
+//
+//            FileOutputStream fos = null;
+//            try {
+//                fos = new FileOutputStream(Paths.get(Main1.OUT + "\\" + s).toFile());
+//                xwpfDocument.write(fos);
+//                fos.close();
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//    }
 }
